46230e348497e74019296133c0f2f61bbd44aeb9
Melikovgraf <Melikovgraf@yandex.ru>
2022-03-15 10:29:27 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+const val MIN_COMMISSION = 35F
+const val COMMISSION = 0.0075F
+fun main() {
+    print("Input amount: ")
+    val amount = readLine()?.toFloat() ?: return
+    val resultCommission = amount * COMMISSION
+    val result = if (resultCommission <= MIN_COMMISSION)
+        MIN_COMMISSION
+    else resultCommission
+    val resultFinish: Int = result.toInt()
+    val resultCent = if (resultCommission <= MIN_COMMISSION)
+        0
+    else resultCommission - resultFinish
+    println("Коммиссия составит $resultFinish рублей $resultCent копеек")
+}
