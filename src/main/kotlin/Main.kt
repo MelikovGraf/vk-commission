53f29a15267c45e39d29f68d30d6a245026b0406
MelikovGraf <98654937+MelikovGraf@users.noreply.github.com>
2022-03-16 11:17:11 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,19 @@ const val MIN_COMMISSION = 35F
 const val NULL = 0F
 const val COMMISSION = 0.0075F
 fun main() {
-    print("Input amount: ")
-    val amount = readLine()?.toFloat() ?: return
-    val resultCommission = amount * COMMISSION
-    val result = if (resultCommission <= NULL)
-        NULL
-    else if (resultCommission <= MIN_COMMISSION)
+    while (true) {
+        print("Input amount: ")
+        val amount = readLine()?.toFloat() ?: return
+        val resultCommission = amount * COMMISSION
+        val result = if (resultCommission <= NULL || amount < 4667)
+            NULL
+        else if (resultCommission <= MIN_COMMISSION)
             MIN_COMMISSION
-    else resultCommission
-    val resultFinish: Int = result.toInt()
-    val resultCent = if (resultCommission <= MIN_COMMISSION)
-        0
-    else resultCommission - resultFinish
-    println("Коммиссия составит $resultFinish рублей $resultCent копеек")
+        else resultCommission
+        val resultFinish: Int = result.toInt()
+        val resultCent = if (resultCommission <= MIN_COMMISSION)
+            0
+        else resultCommission - resultFinish
+        println("Коммиссия составит $resultFinish рублей $resultCent копеек")
+    }
 }
