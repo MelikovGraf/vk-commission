2dc277d270e536f4fa728a7c792951aead7beb58
Graf <98654937+MelikovGraf@users.noreply.github.com>
2022-04-13 09:11:07 +0300

    Update mainFour.kt

--- src/main/kotlin/mainFour.kt
+++ src/main/kotlin/mainFour.kt
@@ -1,34 +1,26 @@
-const val JUST_NOW = 60
-const val MINUTE = 3600
-const val HOUR = 86400
-const val DAY = 172800
-const val YESTERDAY = 259200
+const val JUST_NOW = 0
+const val MINUTE = 60
+const val HOUR = 3600
+const val DAY = 86400
+const val YESTERDAY = 172800
 const val OTHER = 100_000_000
-const val NULL = 0
-
 
 fun main() {
     while (true) {
-        ago(JUST_NOW, MINUTE, HOUR, DAY, YESTERDAY, OTHER, NULL)
+        print("Введите кол-во секунд, проведенные пользователем: ")
+        val second = readLine()?.toInt() ?: return
+        timeOnline(second)
     }
 }
 
-fun ago(
-    JUST_NOW: Int,
-    MINUTE: Int,
-    HOUR: Int,
-    DAY: Int,
-    YESTERDAY: Int,
-    OTHER: Int,
-    NULL: Int) {
-    print("Введите кол-во секунд, проведенные пользователем: ")
-    val second = readLine()?.toInt() ?: return
-    val result = if (second < 0) NULL
-    else if (second < JUST_NOW) JUST_NOW
-    else if (second < MINUTE) MINUTE
-    else if (second < HOUR) HOUR
-    else if (second < DAY) DAY
-    else if (second < YESTERDAY) YESTERDAY
+fun timeOnline(
+    second: Int
+) {
+    val result = if (second < MINUTE) JUST_NOW
+    else if (second < HOUR) MINUTE
+    else if (second < DAY) HOUR
+    else if (second < YESTERDAY) DAY
+    else if (second < OTHER) YESTERDAY
     else OTHER
     when (result) {
         JUST_NOW -> println("Только что был в сети")
@@ -44,7 +36,7 @@ fun ago(
 }
 
 fun minute(
-    count: Int
+    count: Int,
 ): String {
     val total = count / 60
     return if (total > 5 && total < 21) "$total минут "
@@ -54,9 +46,9 @@ fun minute(
 }
 
 fun hour(
-    count: Int
+    count: Int,
 ): String {
-    val total = count / 60 / 24
+    val total = count / 60 / 60
     return if (total > 5 && total < 21) "$total часов "
     else if (total % 10 == 2 || total % 10 == 3 || total % 10 == 4) "$total часа "
     else if (total == 1 || total % 10 == 1) "$total час "
